9c8b6de61f386d26c5c83dc6671845bd173efeba
Anna-Milya <rayinframe@yandex.ru>
2022-10-01 23:05:38 +0300

    burger menu

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const mobileNavButton = document.querySelector('.mobile-nav-button');
+const mobileNavIcon = document.querySelector('.mobile-nav-button__icon');
+mobileNavButton.addEventListener('click', function () {
+   mobileNavIcon.classList.toggle('active');
+})
